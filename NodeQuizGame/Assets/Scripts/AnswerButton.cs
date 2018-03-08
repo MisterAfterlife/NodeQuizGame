de3a27f0b8f91789f3e7167a6afc5e40910bd20d
Saavedra <aasaavedra@labs.neit.edu>
2018-03-08 13:07:02 -0500

    Updated files 3/8

---
+++ NodeQuizGame/Assets/Scripts/AnswerButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour {
+    public Text answerText;
+    private AnswerData answerData;
+    private GameController gameContoller;
+
+	// Use this for initialization
+	void Start () {
+        gameContoller = FindObjectOfType<GameController>();
+	}
+	
+	public void Setup (AnswerData data) {
+        answerData = data;
+        answerText.text = answerData.answerText;
+	}
+
+    public void Clicked(){
+        gameContoller.AnswerClicked(answerData.isCorrect);
+    }
+}
